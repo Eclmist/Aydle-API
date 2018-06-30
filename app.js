ad589a52d5111c920218c55761cfca0a89f36578
yue peng <marcusheadgotscarm16@gmail.com>
2018-06-30 15:37:19 +0800

    changed cors settings

--- app.js
+++ app.js
@@ -5,13 +5,18 @@ var cors = require('cors')
 var app = express();
 var serv = require('http').Server(app);
 
+var corsOptions = {
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
 app.use(cors())
 
 app.get('/',function(req,res){
 	res.sendFile(__dirname + '/client/index.html');
 });	
 
-app.get('/room/:id', function(req,res)
+app.get('/room/:id', cors(corsOptions), function(req,res)
 {
   let result = false;
   let code = req.params.id;
