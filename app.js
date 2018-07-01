b8eb2ff3334a5f14d13b05953a5d6fc1f832392a
yue peng <marcusheadgotscarm16@gmail.com>
2018-07-01 23:08:56 +0800

    made bool value a string

--- app.js
+++ app.js
@@ -11,14 +11,14 @@ var corsOptions = {
 }
 
 app.use(cors())
-/*
+
  app.use(function(req, res, next) {
 	 res.header("Access-Control-Allow-Origin", "*");
-	 res.header("Access-Control-Allow-Credentials" , true);
+	 res.header("Access-Control-Allow-Credentials" , 'true');
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
    next();	
  });
-*/
+
 app.get('/',function(req,res){
 	//res.sendFile(__dirname + '/client/index.html');
 	res.send('running');
