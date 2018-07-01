c3091e7224851179f50382e42b5d16b077b17903
Samuel Ng <eclmist@yahoo.com>
2018-07-02 02:16:20 +0800

    this better fucking work

--- app.js
+++ app.js
@@ -11,14 +11,6 @@ var corsOptions = {
 }
 app.use(cors(corsOptions));
 
-app.use(function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Credentials" , 'true');
-	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();	
-});
-
 app.get('/',function(req,res){
 	//res.sendFile(__dirname + '/client/index.html');
 	res.send('running');
