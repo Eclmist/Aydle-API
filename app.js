ef948c14ae06e7604aa3a3f3f6b309adb74e2203
Samuel Ng <eclmist@yahoo.com>
2018-07-04 13:57:36 +0800

    debug

--- app.js
+++ app.js
@@ -117,6 +117,7 @@ io.sockets.on('connection', function(socket)
 	// route the user to another socket channel
 	socket.on('requestJoin',function(code,playerID,successCallback,failureCallback)
 	{
+		successCallback()
 		
 		if(gamerooms[code] !== undefined)
 		{
